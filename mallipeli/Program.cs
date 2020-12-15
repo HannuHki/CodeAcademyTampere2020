d40d2f84ba435ef785654b269c2526847e541b87
HannuHki <hannu_hautamaki@hotmail.com>
2020-12-15 11:13:04 +0200

    Uudet lisäykset

---
+++ mallipeli/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace mallipeli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tervetuloa Arvaa luku -peliin!");
+            Random rnd = new Random();
+            int oikeaVastaus = rnd.Next(1, 21);
+            Console.WriteLine(oikeaVastaus);
+
+            for (int arvausluku=0; arvausluku<3; arvausluku++)
+            {
+                Console.WriteLine("Anna arvauksesi välillä 1-20:");
+                int arvaus = int.Parse(Console.ReadLine());
+                if (arvaus < oikeaVastaus)
+                {
+                    Console.WriteLine("Oikea luku on suurempi!");
+                }        
+                if (arvaus > oikeaVastaus)
+                {
+                    Console.WriteLine("Oikea luku on pienempi!");
+                }
+                if (arvaus == oikeaVastaus)
+                {
+                    Console.WriteLine("Oikea luku"); 
+                    break;
+                }
+            }
+        }
+    }
+}
